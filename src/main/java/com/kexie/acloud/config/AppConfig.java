aa13a3cc2ab7e20306d3f9b86ae10d596eb3e5d4
zojian <hellozojian@gmail.com>
2017-04-26 16:48:21 +0800

    添加multipart解析器

--- src/main/java/com/kexie/acloud/config/AppConfig.java
+++ src/main/java/com/kexie/acloud/config/AppConfig.java
@@ -3,25 +3,18 @@ package com.kexie.acloud.config;
 /**
  * Created by zojian on 2017/4/25.
  */
-import org.springframework.context.MessageSource;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.ComponentScan;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.Import;
-import org.springframework.context.support.ReloadableResourceBundleMessageSource;
-import org.springframework.web.servlet.LocaleResolver;
+import org.springframework.context.annotation.*;
+import org.springframework.web.multipart.MultipartResolver;
+import org.springframework.web.multipart.support.StandardServletMultipartResolver;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
-import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
-import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
-import org.springframework.web.servlet.i18n.CookieLocaleResolver;
-import org.springframework.web.servlet.i18n.LocaleChangeInterceptor;
 import org.springframework.web.servlet.view.InternalResourceViewResolver;
 
-import java.util.Locale;
+import java.io.IOException;
 
 @Configuration
-@ComponentScan("com.kexie.acloud")
+@ComponentScan(basePackages = {"com.kexie.acloud"},
+        excludeFilters = { @ComponentScan.Filter(type = FilterType.ANNOTATION, value = Configuration.class)})
 @Import(DBConfig.class)
 @EnableWebMvc
 public class AppConfig extends WebMvcConfigurerAdapter  {
@@ -32,29 +25,34 @@ public class AppConfig extends WebMvcConfigurerAdapter  {
         resolver.setSuffix(".jsp");
         return resolver;
     }
+    //配置multipart解析器
     @Bean
-    public MessageSource messageSource() {
-        ReloadableResourceBundleMessageSource messageSource = new ReloadableResourceBundleMessageSource();
-        messageSource.setBasename("/WEB-INF/i18/messages");
-        messageSource.setDefaultEncoding("UTF-8");
-        return messageSource;
+    public MultipartResolver multipartResolver()throws IOException{
+        return new StandardServletMultipartResolver();
     }
-    @Bean
-    public LocaleResolver localeResolver(){
-        CookieLocaleResolver resolver = new CookieLocaleResolver();
-        resolver.setDefaultLocale(new Locale("en"));
-        resolver.setCookieName("myLocaleCookie");
-        resolver.setCookieMaxAge(4800);
-        return resolver;
-    }
-    @Override
-    public void addInterceptors(InterceptorRegistry registry) {
-        LocaleChangeInterceptor interceptor = new LocaleChangeInterceptor();
-        interceptor.setParamName("mylocale");
-        registry.addInterceptor(interceptor);
-    }
-    @Override
-    public void addResourceHandlers(ResourceHandlerRegistry registry) {
-        registry.addResourceHandler("/app-resources/**").addResourceLocations("/resources/");
-    }
+//    @Bean
+//    public MessageSource messageSource() {
+//        ReloadableResourceBundleMessageSource messageSource = new ReloadableResourceBundleMessageSource();
+//        messageSource.setBasename("/WEB-INF/i18/messages");
+//        messageSource.setDefaultEncoding("UTF-8");
+//        return messageSource;
+//    }
+//    @Bean
+//    public LocaleResolver localeResolver(){
+//        CookieLocaleResolver resolver = new CookieLocaleResolver();
+//        resolver.setDefaultLocale(new Locale("en"));
+//        resolver.setCookieName("myLocaleCookie");
+//        resolver.setCookieMaxAge(4800);
+//        return resolver;
+//    }
+//    @Override
+//    public void addInterceptors(InterceptorRegistry registry) {
+//        LocaleChangeInterceptor interceptor = new LocaleChangeInterceptor();
+//        interceptor.setParamName("mylocale");
+//        registry.addInterceptor(interceptor);
+//    }
+//    @Override
+//    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+//        registry.addResourceHandler("/app-resources/**").addResourceLocations("/resources/");
+//    }
 }
